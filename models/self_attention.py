3b00e82dd6e4c2135acd84dba3ae70549b013753
liuyalinrich~ <tanguy.liuyalin@gmail.com>
2023-05-06 18:46:21 +0800

    save the note and TODO

--- models/self_attention.py
+++ models/self_attention.py
@@ -17,24 +17,25 @@ class SelfAttentionNet(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        out = self.embedding(x)
-        out = out.permute(1, 0, 2)
+        out = self.embedding(x) #这个就相当于将维度提高到了 hidden_size上面去
+        out = out.permute(1, 0, 2) #这里是将对应的维度调整过去，从而方式出现问题
         out, _ = self.attention(out, out, out)
         out = out.permute(1, 0, 2)
         out = self.relu(out)
         out = self.fc(out)
         return out.squeeze()
 
+if __name__ == '__main__':
 
-# 定义测试Self-Attention网络
-input_size = 10
-hidden_size = 20
-num_heads = 2
-num_layers = 2
-seq_len = 5
-batch_size = 2
+    # 定义测试Self-Attention网络
+    input_size = 10
+    hidden_size = 100
+    num_heads = 2
+    num_layers = 2
+    seq_len = 5
+    batch_size = 2
 
-model = SelfAttentionNet(input_size, hidden_size, num_heads, num_layers)
-x = torch.randn(seq_len, batch_size, input_size)
-out = model(x)
-print(out.shape)  # (2,)
+    model = SelfAttentionNet(input_size, hidden_size, num_heads, num_layers)
+    x = torch.randn(seq_len, batch_size, input_size) # 5 2 10
+    out = model(x)
+    print(x.shape,out.shape)  # (2,)
