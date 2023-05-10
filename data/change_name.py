ff4327e4d777bf01fb82ee69bf81555f249c2820
liuyalinrich~ <tanguy.liuyalin@gmail.com>
2023-05-10 14:58:16 +0800

    generate the new data for test. with some really samll dataset to test faster

--- data/change_name.py
+++ data/change_name.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 def change_data_name(new_name):
-    path = '../data/Wind farm site 1 (Nominal capacity-99MW).xlsx'
     columns = {'Time(year-month-day h:m:s)' : 'time',
                'Wind speed at height of 10 meters (m/s)' : 'wind10',
                'Wind direction at height of 10 meters (˚)' : 'angle10',
@@ -14,8 +13,13 @@ def change_data_name(new_name):
                'Relative humidity (%)' : 'humidity',
                'Power (MW)' : 'power'
            }
+    drop_list = ['Wind speed - at the height of wheel hub (m/s)', 'Wind speed - at the height of wheel hub (˚)']
+
     path = os.path.join('../data',new_name)
     df = pd.read_excel(path)
-    df.rename(columns=columns, inplace=True)
+    df = df.rename(columns=columns, inplace=True)
+    df.drop(drop_list, axis=1, inplace=True)
     df.to_csv('../data/data_changed_name.csv')
     print(f'name changed and dat saved to {path}')
+
+change_data_name('Wind farm site 1 (Nominal capacity-99MW).xlsx')
