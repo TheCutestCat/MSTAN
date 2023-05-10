4ee9b134c1044ef3fc80798adea63bbffb2be5a6
liuyalinrich~ <tanguy.liuyalin@gmail.com>
2023-05-10 08:48:25 +0800

    bad result of the MSTAN with error of about 6 this is too large

--- train.py
+++ train.py
@@ -2,9 +2,9 @@ from Models.MSTAN_value import trainer_MSTAN_value
 from config import *
 
 if __name__ == '__main__':
-    mytrainer = trainer_MSTAN_value(learning_rate= 0.002)
+    mytrainer = trainer_MSTAN_value(learning_rate= 0.01)
     # mytrainer.load(name = 'test_loss')
-    mytrainer.train(epoch= 10,early_stop_patience= 2)
+    mytrainer.train(epoch= 10,early_stop_patience= 4)
     mytrainer.save(name = 'test_loss')
     # 我们将训练和测试集分开
     # mytrainer.get_show_data(index= 1)
